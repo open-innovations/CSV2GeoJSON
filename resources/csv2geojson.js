7191dc6498fd74bfe0bc460c4254b90aab9012ce
Stuart Lowe <299787+slowe@users.noreply.github.com>
2025-08-05 15:05:48 +0100

    Add Wd24CD and WD25CD

--- resources/csv2geojson.js
+++ resources/csv2geojson.js
@@ -220,7 +220,7 @@ S(document).ready(function(){
 		//this.datatypes = [{"label":"string","ref":"http://www.w3.org/2001/XMLSchema#string"},{"label":"integer","ref":"http://www.w3.org/2001/XMLSchema#int"},{"label":"float","ref":"http://www.w3.org/2001/XMLSchema#float"},{"label":"double","ref":"http://www.w3.org/2001/XMLSchema#double"},{"label":"URL","ref":"http://www.w3.org/2001/XMLSchema#anyURI"},{"label":"boolean","ref":"http://www.w3.org/2001/XMLSchema#boolean"},{"label":"non-positive integer","ref":"http://www.w3.org/2001/XMLSchema#nonPositiveInteger"}, {"label":"positive integer","ref":"http://www.w3.org/2001/XMLSchema#positiveInteger"}, {"label":"non-negative integer","ref":"http://www.w3.org/2001/XMLSchema#nonNegativeInteger"}, {"label":"negative integer","ref":"http://www.w3.org/2001/XMLSchema#negativeInteger"},{"label":"date","ref":"http://www.w3.org/2001/XMLSchema#date"}, {"label":"date & time","ref":"http://www.w3.org/2001/XMLSchema#dateTime"},{"label":"year","ref":"http://www.w3.org/2001/XMLSchema#gYear"},{"label":"year & month","ref":"http://www.w3.org/2001/XMLSchema#gYearMonth"},{"label":"time","ref":"http://www.w3.org/2001/XMLSchema#time "}];
 		this.datatypes = [{"label":"string","ref":"http://www.w3.org/2001/XMLSchema#string"},{"label":"integer","ref":"http://www.w3.org/2001/XMLSchema#int"},{"label":"float","ref":"http://www.w3.org/2001/XMLSchema#float"},{"label":"double","ref":"http://www.w3.org/2001/XMLSchema#double"},{"label":"URL","ref":"http://www.w3.org/2001/XMLSchema#anyURI"},{"label":"boolean","ref":"http://www.w3.org/2001/XMLSchema#boolean"},{"label":"date","ref":"http://www.w3.org/2001/XMLSchema#date"}, {"label":"datetime","ref":"http://www.w3.org/2001/XMLSchema#dateTime"},{"label":"year","ref":"http://www.w3.org/2001/XMLSchema#gYear"},{"label":"time","ref":"http://www.w3.org/2001/XMLSchema#time "}];
 
-		this.geographies = {'LSOA21CD':{},'LSOA11CD':{},'LSOA01CD':{},'MSOA21CD':{},'MSOA11CD':{},'WD19CD':{},'WD20CD':{},'WD21CD':{},'WD22CD':{},'WD23CD':{},'PCON17CD':{},'LAD19CD':{},'LAD20CD':{},'LAD21CD':{},'LAD23CD':{},'CAUTH21CD':{}};
+		this.geographies = {'LSOA21CD':{},'LSOA11CD':{},'LSOA01CD':{},'MSOA21CD':{},'MSOA11CD':{},'WD19CD':{},'WD20CD':{},'WD21CD':{},'WD22CD':{},'WD23CD':{},'WD24CD':{},'WD25CD':{},'PCON17CD':{},'LAD19CD':{},'LAD20CD':{},'LAD21CD':{},'LAD23CD':{},'CAUTH21CD':{}};
 		this.messages = [];
 
 		// If we provided a filename we load that now
@@ -423,10 +423,16 @@ S(document).ready(function(){
 				}else if(p < 0 && (this.data.fields.title[c].toUpperCase() == "MSOA11CD")){
 					p = c;
 					this.geotype = "MSOA11CD";
-				}else if(p < 0 && (this.data.fields.title[c].toUpperCase() == "WARD" || this.data.fields.title[c].toUpperCase() == "WD23CD")){
+				}else if(p < 0 && (this.data.fields.title[c].toUpperCase() == "WARD" || this.data.fields.title[c].toUpperCase() == "WD25CD")){
 					p = c;
-					this.geotype = "WD21CD";
+					this.geotype = "WD25CD";
 					//this.patterns = [/^E05[0-9]{6}$/]
+				}else if(p < 0 && (this.data.fields.title[c].toUpperCase() == "WD24CD")){
+					p = c;
+					this.geotype = "WD24CD";
+				}else if(p < 0 && (this.data.fields.title[c].toUpperCase() == "WD23CD")){
+					p = c;
+					this.geotype = "WD23CD";
 				}else if(p < 0 && (this.data.fields.title[c].toUpperCase() == "WD22CD")){
 					p = c;
 					this.geotype = "WD22CD";
